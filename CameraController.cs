d655be1bb262987390e66f9d59bbfac675d287a1
sofawitch <sofawitchdev@gmail.com>
2020-12-07 21:05:18 -0500

    Simple Orthographic Camera Controller
    This is a simple orthographic camera controller. I'll make seperate scripts for the actual controls.

---
+++ CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera maincamera;
+    public float zoomOutMin = 1;
+    public float zoomOutMax = 8;
+    public float increment;
+
+    void Update()
+    {
+        var zoom = -1;
+        maincamera.orthographicSize += Input.mouseScrollDelta.y * zoom;
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
+    }
+}
